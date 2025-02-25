4e78f2089b90005ce405603354f897e4f8f6b0b0
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2025-02-25 22:04:07 +0000

    task 4: image picker and media library integrated onto add-post tab

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,17 +1,74 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button, TextInput, Image, Pressable } from "react-native";
+import { useState } from "react";
+import { useImagePicker } from "@/hooks/useImagePicker";
+import { BorderlessButton } from "react-native-gesture-handler";
 
 export default function Page() {
+    const [caption, setCaption] = useState<string>("");
+    const {image, openImagePicker, reset} = useImagePicker();
+
     return (
-        <View style={styles.header}>
-            <Text>Add Post Page</Text>
+        <View style={styles.container}>
+            <View style={styles.footerContainer}>
+                {!image && (
+                    <Button
+                        title="Choose a photo"
+                        onPress={openImagePicker}
+                    />
+                )}
+                {image && (
+                    <View style={{ flex: 1, gap: 16, alignItems: "center" }}>
+                        <Image source={{ uri: image }} style={styles.image} />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Add a caption"
+                            value={caption}
+                            onChangeText={setCaption}
+                        />
+                        <Pressable style={styles.saveButton} onPress={() => alert("Save")}>
+                            <Text style={{ color:"white"}}>Save</Text>
+                        </Pressable>
+
+                        <Pressable style={styles.resetButton} onPress={reset}>
+                            <Text style={{ fontWeight:"600" }}>Reset</Text>
+                        </Pressable>
+                    </View>
+                )}
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({ 
-    header: {
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    footerContainer: {
+        padding: 16,
+    },
+    input : {
+        borderWidth: 2,
+        borderColor: "#1ED2AF",
+        borderRadius: 5,
+        padding: 10,
+        width: "100%", 
+    },
+    image: {
+        width: 300,
+        height: 300,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    saveButton: {
+        backgroundColor: "#1ED2AF",
+        width: "80%",
+        borderRadius: 8,
+        alignItems: "center",
+        paddingVertical: 15,
+    },
+    resetButton: {
+        paddingTop: 10,
     }
 });
---
+++ hooks/useImagePicker.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { usePermissions } from "expo-media-library";
+import * as ImagePicker from "expo-image-picker";
+
+export function useImagePicker() {
+     const [image, setImage] = useState<string | undefined>(undefined);
+     const [status, requestPermission] = usePermissions();
+
+     async function openImagePicker() {
+        // Permission request
+        if(status === null){
+            const permission = await requestPermission();
+            if(permission.granted === false){
+                alert("You need to grant permission to access your photos")
+                return;
+            }
+        }
+
+        // Once given permission, open image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+        });
+        // Validates that user selected an image
+        if(!result.canceled){
+            setImage(result.assets[0].uri); // Update state variable to use that image
+        }
+     }
+
+     function reset() {
+        alert("reset");
+     }
+
+     return { image, openImagePicker, reset };
+}
